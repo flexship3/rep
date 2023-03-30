0c8b3af28836af9129b2c64bbf07cf00c2daeda3
nielsvg95 <22121226@student.hhs.nl>
2023-03-30 14:40:52 +0200

    test

--- Shipflex_offerte/src/Main.java
+++ Shipflex_offerte/src/Main.java
@@ -17,14 +17,39 @@ class Oiltanker implements Boat{
     BoatPart navAndComSys = new BoatPart("Navigation and communication system", "");
     BoatPart safetyEquipment = new BoatPart("Safety equipment", "");
 }
-class Sailboat implements Boat{}
-class Cruiseship implements Boat{}
-class Fishingvessel implements Boat{}
+class Sailboat implements Boat{
+    BoatPart hull = new BoatPart("Hull","");
+    BoatPart mastAndRigging = new BoatPart("Mast and rigging","");
+    BoatPart sails = new BoatPart("Sails","");
+    BoatPart keel = new BoatPart("Keel","");
+    BoatPart steeringSystem = new BoatPart("Steering system","");
+    BoatPart electricalSystem = new BoatPart("Electrical system","");
+    BoatPart interiorFurnishings = new BoatPart("Interior furnishings","");
+    BoatPart safetyEquipment = new BoatPart("Safety equipment","");
+
+}
+class Cruiseship implements Boat {
+    BoatPart hull = new BoatPart("Hull", "");
+    BoatPart engines = new BoatPart("Engines", "");
+    BoatPart propellers = new BoatPart("Propellers", "");
+    BoatPart accommodations = new BoatPart("Accommodations", "");
+    BoatPart diningAndEntertainmentFacilities = new BoatPart("Dining and entertainment facilities", "");
+    BoatPart safetyEquipment = new BoatPart("Safety equipment", "");
+    BoatPart navigationAndCommunicationSystems = new BoatPart("Navigation and communication systems", "");
+}
+class Fishingvessel implements Boat{
+    BoatPart hull = new BoatPart("Hull","");
+    BoatPart engine = new BoatPart("Engine","");
+    BoatPart fishingGearAndEquipment = new BoatPart("Fishing gear and equipment","");
+    BoatPart navigationAndCommunicationSystems = new BoatPart("Navigation and communication systems","");
+    BoatPart deckMachineryAndEquipment = new BoatPart("Deck machinery and equipment","");
+    BoatPart safetyEquipment = new BoatPart("Safety equipment","");
+}
 
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Kut project");
+        System.out.println("");
     }
 
 }
