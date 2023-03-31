93280f025dd30ed466de7303ea39e31bfee6b029
Renko <Renkob2005@gmail.com>
2023-03-31 14:34:30 +0200

    heb alle "average prices" ingevuld voor elk onderdeel per boat

--- Shipflex_offerte/src/Main.java
+++ Shipflex_offerte/src/Main.java
@@ -3,58 +3,58 @@ import java.util.Arrays;
 
 interface Boat{}
 class Speedboat implements Boat{
-    BoatPart hull = new BoatPart("Hull", "" );
-    BoatPart outboardMotor = new BoatPart("Outboard Motor", "" );
-    BoatPart fuelTank = new BoatPart("Fuel Tank", "" );
-    BoatPart steeringSystem = new BoatPart("Steering system", "" );
-    BoatPart electricalSystem = new BoatPart("Electrical system", "" );
-    BoatPart upholstery = new BoatPart("Upholstery", "" );
-    BoatPart safetyEquipment = new BoatPart("Safety equipment", "" );
+    BoatPart hull = new BoatPart("Hull", "$5,000 to $20,000" );
+    BoatPart outboardMotor = new BoatPart("Outboard Motor", "$5,000 to $15,000" );
+    BoatPart fuelTank = new BoatPart("Fuel Tank", "$500 to $1,500" );
+    BoatPart steeringSystem = new BoatPart("Steering system", "$1,000 to $3,000" );
+    BoatPart electricalSystem = new BoatPart("Electrical system", "$1,000 to $3,000" );
+    BoatPart upholstery = new BoatPart("Upholstery", "$1,000 to $3,000" );
+    BoatPart safetyEquipment = new BoatPart("Safety equipment", "$500 to $1,500" );
     ArrayList<BoatPart> boatParts = new ArrayList<BoatPart>(Arrays.asList(hull, outboardMotor, fuelTank, steeringSystem,
             electricalSystem, upholstery,safetyEquipment)); // De boot heeft een arraylist met BoatPart objecten
 }
 class Oiltanker implements Boat{
-    BoatPart hull = new BoatPart("Hull", "");
-    BoatPart engines = new BoatPart("Engines", "");
-    BoatPart propulsionSystem = new BoatPart("Propulsion system", "");
-    BoatPart cargoTanks = new BoatPart("Cargo tanks", "");
-    BoatPart pumpsAndValves = new BoatPart("Pumps and valves", "");
-    BoatPart navAndComSys = new BoatPart("Navigation and communication system", "");
-    BoatPart safetyEquipment = new BoatPart("Safety equipment", "");
+    BoatPart hull = new BoatPart("Hull", "$100 million to $500 million or more");
+    BoatPart engines = new BoatPart("Engines", "$50 million to $100 million or more");
+    BoatPart propulsionSystem = new BoatPart("Propulsion system", "$20 million to $50 million or more");
+    BoatPart cargoTanks = new BoatPart("Cargo tanks", "$20 million to $50 million or more");
+    BoatPart pumpsAndValves = new BoatPart("Pumps and valves", "$5 million to $20 million or more");
+    BoatPart navAndComSys = new BoatPart("Navigation and communication system", "$10 million to $50 million or more");
+    BoatPart safetyEquipment = new BoatPart("Safety equipment", "$10 million to $50 million or more");
     ArrayList<BoatPart> boatParts = new ArrayList<BoatPart>(Arrays.asList(hull, engines, propulsionSystem,
             cargoTanks, pumpsAndValves, navAndComSys, safetyEquipment));
 }
 class Sailboat implements Boat{
-    BoatPart hull = new BoatPart("Hull","");
+    BoatPart hull = new BoatPart("Hull","$5,000 to $30,000");
     BoatPart mastAndRigging = new BoatPart("Mast and rigging","");
-    BoatPart sails = new BoatPart("Sails","");
-    BoatPart keel = new BoatPart("Keel","");
-    BoatPart steeringSystem = new BoatPart("Steering system","");
-    BoatPart electricalSystem = new BoatPart("Electrical system","");
-    BoatPart interiorFurnishings = new BoatPart("Interior furnishings","");
-    BoatPart safetyEquipment = new BoatPart("Safety equipment","");
+    BoatPart sails = new BoatPart("Sails","$10,000 to $15,000");
+    BoatPart keel = new BoatPart("Keel","$10,000 to $15,000");
+    BoatPart steeringSystem = new BoatPart("Steering system","$2,000 to $5,000");
+    BoatPart electricalSystem = new BoatPart("Electrical system","$2,000 to $5,000");
+    BoatPart interiorFurnishings = new BoatPart("Interior furnishings","$1,000 to $3,000");
+    BoatPart safetyEquipment = new BoatPart("Safety equipment","$1,000 to $3,000");
     ArrayList<BoatPart> boatParts = new ArrayList<BoatPart>(Arrays.asList(hull, mastAndRigging, sails,
             keel, steeringSystem, electricalSystem, interiorFurnishings, safetyEquipment));
 
 }
 class Cruiseship implements Boat {
-    BoatPart hull = new BoatPart("Hull", "");
-    BoatPart engines = new BoatPart("Engines", "");
-    BoatPart propellers = new BoatPart("Propellers", "");
-    BoatPart accommodations = new BoatPart("Accommodations", "");
-    BoatPart diningAndEntertainmentFacilities = new BoatPart("Dining and entertainment facilities", "");
-    BoatPart safetyEquipment = new BoatPart("Safety equipment", "");
-    BoatPart navigationAndCommunicationSystems = new BoatPart("Navigation and communication systems", "");
+    BoatPart hull = new BoatPart("Hull", "$300 million to $1 billion or more");
+    BoatPart engines = new BoatPart("Engines", "$100 million or more");
+    BoatPart propellers = new BoatPart("Propellers", "$5 million to $20 million or more");
+    BoatPart accommodations = new BoatPart("Accommodations", "$50,000 to $100,000 or more per suite");
+    BoatPart diningAndEntertainmentFacilities = new BoatPart("Dining and entertainment facilities", "$2 million to $5 million or more per facility");
+    BoatPart safetyEquipment = new BoatPart("Safety equipment", "$10 million to $20 million or more");
+    BoatPart navigationAndCommunicationSystems = new BoatPart("Navigation and communication systems", "$50 million or more");
     ArrayList<BoatPart> boatParts = new ArrayList<BoatPart>(Arrays.asList(hull, engines, propellers,
             accommodations, diningAndEntertainmentFacilities, safetyEquipment, navigationAndCommunicationSystems));
 }
 class Fishingvessel implements Boat{
-    BoatPart hull = new BoatPart("Hull","");
-    BoatPart engine = new BoatPart("Engine","");
-    BoatPart fishingGearAndEquipment = new BoatPart("Fishing gear and equipment","");
-    BoatPart navigationAndCommunicationSystems = new BoatPart("Navigation and communication systems","");
-    BoatPart deckMachineryAndEquipment = new BoatPart("Deck machinery and equipment","");
-    BoatPart safetyEquipment = new BoatPart("Safety equipment","");
+    BoatPart hull = new BoatPart("Hull","$50,000 to $200,000");
+    BoatPart engine = new BoatPart("Engine","$50,000 to $100,000 or more");
+    BoatPart fishingGearAndEquipment = new BoatPart("Fishing gear and equipment","$10,000 to $50,000 or more");
+    BoatPart navigationAndCommunicationSystems = new BoatPart("Navigation and communication systems","$5,000 to $20,000 or more");
+    BoatPart deckMachineryAndEquipment = new BoatPart("Deck machinery and equipment","$10,000 to $50,000 or more");
+    BoatPart safetyEquipment = new BoatPart("Safety equipment","$5,000 to $20,000 or more");
     ArrayList<BoatPart> boatParts = new ArrayList<BoatPart>(Arrays.asList(hull, engine, fishingGearAndEquipment,
             navigationAndCommunicationSystems, deckMachineryAndEquipment, safetyEquipment));
 }
