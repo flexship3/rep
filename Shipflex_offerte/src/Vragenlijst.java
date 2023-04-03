b4afd0daa76540ae6e639d89c64334fdd5ce769d
Renko Brauckman <renkob2005@gmail.com>
2023-04-03 10:52:34 +0200

    code aangepast om de int price in Boatpart te gebruken

--- Shipflex_offerte/src/Vragenlijst.java
+++ Shipflex_offerte/src/Vragenlijst.java
@@ -8,12 +8,12 @@ public class Vragenlijst {
     public void prompt(ArrayList<BoatPart> boatParts) {
         Scanner scanner = new Scanner(System.in);
         for (int i = 0; i < boatParts.size(); i++) {System.out.printf("How much does the %s cost? average price(%s)", boatParts.get(i).getName(), boatParts.get(i).getInfo());
-            prijslijst.prijzen.add(scanner.nextInt());
+            boatParts.get(i).setPrice(scanner.nextInt());
 
         }
-        for (int i = 0; i < prijslijst.prijzen.size(); i++) {
-            System.out.println(boatParts.get(i).getName() + ": " + prijslijst.prijzen.get(i));
-            this.prijsTotaal += prijslijst.prijzen.get(i);
+        for (int i = 0; i < boatParts.size(); i++) {
+            System.out.println(boatParts.get(i).getName() + ": " + boatParts.get(i).getPrice());
+            this.prijsTotaal += boatParts.get(i).getPrice();
         }// deze method neemt een lijst boatparts en geeft een vragenlijst gebaseerd hierop
         System.out.println("Total: " + prijsTotaal);
 
