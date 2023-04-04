f5a5976bc8b189a475680da34dcbb9d92cc16888
nielsvg95 <22121226@student.hhs.nl>
2023-04-04 10:49:27 +0200

    .txt functionaliteit

--- Shipflex_offerte/src/Main.java
+++ Shipflex_offerte/src/Main.java
@@ -116,11 +116,10 @@ class Fishingvessel implements Boat{
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
-        Scanner scanner = new Scanner(System.in);
         Botenlijst botenlijst = new Botenlijst();
         Vragenlijst vragenlijst = new Vragenlijst();
         Klanten klanten = new Klanten();
-        vragenlijst.kortingVraag(klanten, botenlijst, scanner);
+        vragenlijst.kortingVraag(klanten, botenlijst);
 
     }
 
--- Shipflex_offerte/src/Vragenlijst.java
+++ Shipflex_offerte/src/Vragenlijst.java
@@ -7,7 +7,9 @@ import java.util.Scanner;
 public class Vragenlijst {
     // Prijslijst prijslijst = new Prijslijst();
     double prijsTotaal = 0;
-    public void kortingVraag (Klanten klanten, Botenlijst botenlijst, Scanner scanner) throws FileNotFoundException, UnsupportedEncodingException {
+
+    public void kortingVraag (Klanten klanten, Botenlijst botenlijst) throws FileNotFoundException, UnsupportedEncodingException {
+        Scanner scanner = new Scanner(System.in);
         klanten.Vragen();
         System.out.println(klanten.getDiscountPercentage());
         Kiesboat(botenlijst, scanner, klanten);
@@ -21,19 +23,19 @@ public class Vragenlijst {
             }
             int keuze = scanner.nextInt();
             switch (keuze){
-                case 1: prompt(botenlijst.botenlijst.get(0).getboatParts(), klanten);
+                case 1: prompt(botenlijst.botenlijst.get(0).getboatParts(), klanten, scanner);
                     breakloop = true;
                     break;
-                case 2: prompt(botenlijst.botenlijst.get(1).getboatParts(), klanten);
+                case 2: prompt(botenlijst.botenlijst.get(1).getboatParts(), klanten, scanner);
                     breakloop = true;
                     break;
-                case 3: prompt(botenlijst.botenlijst.get(2).getboatParts(), klanten);
+                case 3: prompt(botenlijst.botenlijst.get(2).getboatParts(), klanten, scanner);
                     breakloop = true;
                     break;
-                case 4: prompt(botenlijst.botenlijst.get(3).getboatParts(), klanten);
+                case 4: prompt(botenlijst.botenlijst.get(3).getboatParts(), klanten, scanner);
                     breakloop = true;
                     break;
-                case 5: prompt(botenlijst.botenlijst.get(4).getboatParts(), klanten);
+                case 5: prompt(botenlijst.botenlijst.get(4).getboatParts(), klanten, scanner);
                     breakloop = true;
                     break;
                 default: System.out.println("Please only give a number between 1-5 for the following boats: ");
@@ -42,9 +44,9 @@ public class Vragenlijst {
         }
     }
 
-    public void prompt(ArrayList<BoatPart> boatParts, Klanten klanten) throws FileNotFoundException, UnsupportedEncodingException {
-        Scanner scanner = new Scanner(System.in);
-        // PrintWriter writer = new PrintWriter("offerte.txt", "UTF-8");
+    public void prompt(ArrayList<BoatPart> boatParts, Klanten klanten, Scanner scanner) throws FileNotFoundException, UnsupportedEncodingException {
+        PrintWriter writer = new PrintWriter("offerte.txt", "UTF-8");
+        writer.println("Flex-Ship offerte." + "\n");
         for (int i = 0; i < boatParts.size(); i++) {
             System.out.printf("How much does the %s cost? average price: (%s)", boatParts.get(i).getName(), boatParts.get(i).getInfo());
             boatParts.get(i).setPrice(scanner.nextInt());
@@ -52,11 +54,11 @@ public class Vragenlijst {
         for (int i = 0; i < boatParts.size(); i++) {
             System.out.println(boatParts.get(i).getName() + ": " + boatParts.get(i).getPrice());
             this.prijsTotaal += boatParts.get(i).getPrice();
-            // writer.println(boatParts.get(i).getName() + ": " + boatParts.get(i).getPrice());
+            writer.println(boatParts.get(i).getName() + ": " + boatParts.get(i).getPrice());
         }// deze method neemt een lijst boatparts en geeft een vragenlijst gebaseerd hierop
-        System.out.println("Totaal met korting (" + (100 - klanten.getDiscountPercentage() * 100) + "%): " + prijsTotaal * klanten.getDiscountPercentage());
-        // writer.println("Totaal met korting (" + (100 - klanten.getDiscountPercentage() * 100) + "%): " + prijsTotaal * klanten.getDiscountPercentage());
-        // writer.close();
+        System.out.println("\nTotaal met korting (" + (100 - klanten.getDiscountPercentage() * 100) + "%): " + prijsTotaal * klanten.getDiscountPercentage());
+        writer.println("\nTotaal met korting (" + (100 - klanten.getDiscountPercentage() * 100) + "%): " + prijsTotaal * klanten.getDiscountPercentage());
+        writer.close();
     }
 }
 
